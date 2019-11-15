a358580d07aaf7489500e7ee144848414d5fa966
Michal Kasprzyk <emailer5k@gmail.com>
2019-11-15 10:10:58 +0100

    Add redirects script

---
+++ README.md
@@ -0,0 +1,13 @@
+##REDIRECTS TESTING TOOL   
+
+This is 301 redirects testing tool, for general testing purpose after new site released live.
+
+This tool supports CSV file type, as most popular
+
+How it works:
+- in redirects.py file we can setup which file should be opened:
+    "with open('redirs.txt') as csv_file:" -> put your file instead of redirs.txt and it should read this file
+- after correct path to our file is provided, we can just run script
+- script will push logs into console after each row of CSV is checked
+- every error found will be logged as ERROR in console + additionally all info will be pushed to errors.log file
+- execution timer is applied for checking how much time it took to work out all redirects provided
---
+++ redirects.py
@@ -0,0 +1,52 @@
+import csv
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from termcolor import colored
+
+start_time = time.time()
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')  # Last I checked this was necessary.
+driver = webdriver.Chrome(options=options)
+
+
+def compare():
+    if driver.current_url == row[1]:
+        print("Redir ok at line: " + str(line_count))
+        print("Base URL in " + str(line_count) + ": " + row[0])
+        print("Redirected URL in " + str(line_count) + ": " + row[1])
+    else:
+        file = open("errors.log", "a")
+        file.write("*******************" + "\n" +
+                   "ERROR at line: " + str(line_count) + "\n" +
+                   "Base URL: " + str(line_count) + ": " + row[0] + "\n" +
+                   "Expected URL: " + str(line_count) + ": " + row[1] + "\n" +
+                   "Current URL: " + driver.current_url + "\n" +
+                   "*******************" + "\n" + "\n")
+        print(colored("ERROR at line: " + str(line_count) + " saved in errors.log file", "red"))
+
+
+with open('redirs.txt') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            driver.implicitly_wait(1)
+            driver.get(row[0])
+            driver.implicitly_wait(1)
+            compare()
+            line_count += 1
+        else:
+            driver.implicitly_wait(1)
+            driver.get(row[0])
+            driver.implicitly_wait(1)
+            compare()
+            line_count += 1
+    print(f'Processed {line_count} lines.')
+
+end_time = time.time()
+
+print(colored("Total execution time: {:0.2f} seconds".format(end_time - start_time), "cyan"))
